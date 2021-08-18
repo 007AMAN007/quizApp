6bfe8e2290b0dd2f981c803148f79d9553a04796
007AMAN007 <as47986@gmail.com>
2021-08-18 23:43:38 +0530

    QUIZ APP

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,21 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react";
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Quiz from "react-quiz-component"
+import { quiz } from './quiz';
 
 const BlogPostTemplate = ({ data, location }) => {
+  const [quizResult, setQuizResult] = useState();
+
+  useEffect(() => {
+      if(quizResult) {
+          console.log('quizResult', quizResult);
+      }
+  }, [quizResult]);
+
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const { previous, next } = data
@@ -16,50 +26,13 @@ const BlogPostTemplate = ({ data, location }) => {
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
       />
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
-      </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
+      <Quiz
+        quiz={quiz}
+        shuffle={true}
+        showInstantFeedback={true}
+        continueTillCorrect={true}
+        onComplete={setQuizResult}
+      />
     </Layout>
   )
 }
