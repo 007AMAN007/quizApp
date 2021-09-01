350f15cd28e45605e9c6d08cb65c4256d6782263
007AMAN007 <as47986@gmail.com>
2021-09-01 18:55:48 +0530

    view quiz

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,44 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+// import * as React from "react"
+// import { graphql } from "gatsby"
 
+// import Layout from "../components/layout"
+// import Seo from "../components/seo"
+
+// const NotFoundPage = ({ data, location }) => {
+//   const siteTitle = data.site.siteMetadata.title
+
+//   return (
+//     <Layout location={location} title={siteTitle}>
+//       <Seo title="404: Not Found" />
+//       <h1>404: Not Found</h1>
+//       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+//     </Layout>
+//   )
+// }
+
+// export default NotFoundPage
+
+// export const pageQuery = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//   }
+// `
+
+import React from "react"
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import win from "../util/win_util"
 
 const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  return win ? (
+    <Layout>
+      <h1>You are here!</h1>
+      <h2>But nothing found for you #404</h2>
     </Layout>
-  )
+  ) : null
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
